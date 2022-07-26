a1313ad038ca802e22e4b114950f3105cec9f565
Alex Eaton <alexeaton814@gmail.com>
2022-07-26 08:29:34 -0500

    Complete Lesson 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+  return values.reduce((a,b) => {return a + b},0);
 };
 
-const multiply = function() {
-
+const multiply = function(values) {
+  
+  return values.reduce((a,b) => {return a*b},1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let i = 1;
+  let total = a;
+  while (i < b) {
+    total = total * a;
+    i++
+  }
+  if (b == 0) {total = a}
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	const factorialArray = [];
+  while (num > 0) {
+    factorialArray.push(num);
+    num--;
+  }
+  console.log(factorialArray);
+  return factorialArray.reduce((a,b) => {return a * b;},1);
+  
 };
 
 // Do not edit below this line
