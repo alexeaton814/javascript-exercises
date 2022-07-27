a87c13ace3fdac9cac8362a8aa55d428f6d7bf60
Alex Eaton <alexeaton814@gmail.com>
2022-07-26 21:27:14 -0500

    Complete Lesson 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let validChars = [];
+    for (let i = 65; i <=90; i++){
+        validChars.push(String.fromCharCode(i));
+    }
 
+    input = input.toUpperCase();
+
+    let formattedInput = Array.from(input).filter(letter => {
+        if (validChars.includes(letter) == true) {
+            return true;
+        }
+    })
+
+    console.log(formattedInput);
+    console.log(formattedInput.reverse());
+    for (let i = 0; i < formattedInput.length/2 + 1; i++) {
+        if (formattedInput[i] !== formattedInput.reverse()[i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
