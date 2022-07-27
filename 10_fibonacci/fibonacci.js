3235396e05e051fa8e5e7df39a3499ea5594091e
Alex Eaton <alexeaton814@gmail.com>
2022-07-26 22:01:46 -0500

    Complete Lesson 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    if (typeof(input) != 'number') {
+        input = Number(input);
+    }
 
+    if (input < 0) {
+        return "OOPS";
+    }
+
+    let fibArray = [];
+    for (let i = 0; i < input; i++) {
+        if (i <= 1) {
+            fibArray.push(1);
+        }
+        else {
+            //debugger;
+            console.log(fibArray);
+            let prevNum1 = fibArray[i - 1]; 
+            let prevNum2 = fibArray[i - 2];
+            fibArray.push(prevNum1 + prevNum2)}
+    }
+    console.log(fibArray);
+    return fibArray[fibArray.length - 1];
 };
 
 // Do not edit below this line
